f4c2ca86cea6b226afdf675f05f58c4c7a9075c3
briecodes <hello@brie.work>
2018-05-01 21:36:08 -0400

    nested form defined.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,14 +3,4 @@ class Recipe < ActiveRecord::Base
 
   accepts_nested_attributes_for :ingredients
 
-  def recipe_params
-    params.require(:recipe).permit(
-      :title,
-      ingredients_attributes: [
-        :name,
-        :quantity,
-        :recipe_id
-      ]
-    )
-  end
 end
